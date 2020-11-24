aa55b3be2695a1fcf4dc82937699b71657fb19c1
Elias Lankinen <elias@lankinen.xyz>
2020-11-24 15:28:35 +0200

    cleaned code a little bit

--- Collaborative Building/ContentView.swift
+++ Collaborative Building/ContentView.swift
@@ -26,7 +26,6 @@ let gold = SimpleMaterial(color: .yellow, isMetallic: true)
 let minecraftMode: Bool = true
 
 struct ARViewContainer: UIViewRepresentable {
-//    let origin = AnchorEntity(plane: .horizontal)
     let origin = AnchorEntity(world: [0,0,0])
     @Binding var text: String
     
@@ -39,24 +38,12 @@ struct ARViewContainer: UIViewRepresentable {
         arView.scene.addAnchor(origin)
         
         arView.setupGestures()
-//        let entity = ModelEntity(mesh: .generateBox(size: 0.1), materials: [gold])
-//        let anchor = AnchorEntity(plane: .horizontal)
-//        anchor.addChild(entity)
-//        arView.scene.addAnchor(anchor)
         
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
-}
-
-enum Side {
-    case up
-    case left
-    case front
-    case right
-    case back
 }
 
 extension ARView {
@@ -70,114 +57,114 @@ extension ARView {
         guard let rayResult = self.ray(through: tapLocation) else { return }
         
         let results = self.scene.raycast(origin: rayResult.origin, direction: rayResult.direction)
-        let origin = self.scene.anchors.first
-        let originPosition = origin?.position
-        
-        print("origin position: \(originPosition)")
         
         if let firstResult = results.first {
             var touchPosition = firstResult.position
-            let touchedEntityTranslation = firstResult.entity.transform.translation
+            let touchedEntityPosition = firstResult.entity.transform.translation
             print("touch position: \(touchPosition)")
             print("number of anchors: \(self.scene.anchors.count)")
-            print("touched entity position: \(touchedEntityTranslation)")
+            print("touched entity position: \(touchedEntityPosition)")
             
-            var side: Side? = nil
-            let epsilon: Float = 0.01
-            let boxUp = touchedEntityTranslation.y + (boxSize/2) - epsilon
-            if boxUp < touchPosition.y {
-                side = .up
-            } else {
-                let boxFront = touchedEntityTranslation.z + (boxSize/2) - epsilon
-                let boxBack = touchedEntityTranslation.z - (boxSize/2) + epsilon
-                let boxLeft = touchedEntityTranslation.x + (boxSize/2) - epsilon
-                let boxRight = touchedEntityTranslation.x - (boxSize/2) + epsilon
-                if boxFront < touchPosition.z {
-                    side = .front
-                } else if boxBack > touchPosition.z {
-                    side = .back
-                } else {
-                    if boxLeft > touchPosition.x {
-                        side = .left
-                    } else if boxRight < touchPosition.x {
-                        side = .right
-                    }
-                }
-            }
-            
-            switch side {
-            case .up:
-                print("up")
-                touchPosition.y += boxSize/2
-                if minecraftMode {
-                    touchPosition.x = touchedEntityTranslation.x
-                    touchPosition.z = touchedEntityTranslation.z
-                }
-                break
-            case .front:
-                print("front")
-                touchPosition.z += boxSize/2
-                if minecraftMode {
-                    touchPosition.x = touchedEntityTranslation.x
-                    touchPosition.y = touchedEntityTranslation.y
-                }
-                break
-            case .back:
-                print("back")
-                touchPosition.z -= boxSize/2
-                if minecraftMode {
-                    touchPosition.x = touchedEntityTranslation.x
-                    touchPosition.y = touchedEntityTranslation.y
-                }
-                break
-            case .left:
-                print("left")
-                touchPosition.x -= boxSize/2
-                if minecraftMode {
-                    touchPosition.y = touchedEntityTranslation.y
-                    touchPosition.z = touchedEntityTranslation.z
-                }
-                break
-            case .right:
-                print("right")
-                touchPosition.x += boxSize/2
-                if minecraftMode {
-                    touchPosition.y = touchedEntityTranslation.y
-                    touchPosition.z = touchedEntityTranslation.z
-                }
-                break
-            default:
-                print("nothing")
-                touchPosition.y += boxSize/2
-                if minecraftMode {
-                    touchPosition.x = touchedEntityTranslation.x
-                    touchPosition.z = touchedEntityTranslation.z
-                }
-                break
-            }
+            let side: Side? = recognizeSide(touchedEntityPosition: touchedEntityPosition, touchPosition: touchPosition)
+            setPositionToMiddle(side: side, touchedEntityPosition: touchedEntityPosition, touchPosition: &touchPosition)
             print()
             
             placeCube(at: touchPosition)
         } else {
             let results = self.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any)
-            
             if let firstResult = results.first {
                 let touchPositionInSurface = simd_make_float3(firstResult.worldTransform.columns.3)
-                print("position: \(touchPositionInSurface)")
-                print()
                 placeCube(at: touchPositionInSurface)
+            } else {
+                print("couldn't find a surface")
             }
+        }
+    }
+}
+
+extension ARView {
+    func recognizeSide(touchedEntityPosition: SIMD3<Float>, touchPosition: SIMD3<Float>) -> Side? {
+        var side: Side? = nil
+        let epsilon: Float = 0.01
+        let boxUp = touchedEntityPosition.y + (boxSize/2) - epsilon
+        if boxUp < touchPosition.y {
+            side = .up
+        } else {
+            let boxFront = touchedEntityPosition.z + (boxSize/2) - epsilon
+            let boxBack = touchedEntityPosition.z - (boxSize/2) + epsilon
+            let boxLeft = touchedEntityPosition.x + (boxSize/2) - epsilon
+            let boxRight = touchedEntityPosition.x - (boxSize/2) + epsilon
+            if boxFront < touchPosition.z {
+                side = .front
+            } else if boxBack > touchPosition.z {
+                side = .back
+            } else {
+                if boxLeft > touchPosition.x {
+                    side = .left
+                } else if boxRight < touchPosition.x {
+                    side = .right
+                }
+            }
+        }
+        return side
+    }
+    
+    func setPositionToMiddle (side: Side?, touchedEntityPosition: SIMD3<Float>, touchPosition: inout SIMD3<Float>) {
+        switch side {
+        case .up:
+            print("up")
+            touchPosition.y += boxSize/2
+            if minecraftMode {
+                touchPosition.x = touchedEntityPosition.x
+                touchPosition.z = touchedEntityPosition.z
+            }
+            break
+        case .front:
+            print("front")
+            touchPosition.z += boxSize/2
+            if minecraftMode {
+                touchPosition.x = touchedEntityPosition.x
+                touchPosition.y = touchedEntityPosition.y
+            }
+            break
+        case .back:
+            print("back")
+            touchPosition.z -= boxSize/2
+            if minecraftMode {
+                touchPosition.x = touchedEntityPosition.x
+                touchPosition.y = touchedEntityPosition.y
+            }
+            break
+        case .left:
+            print("left")
+            touchPosition.x -= boxSize/2
+            if minecraftMode {
+                touchPosition.y = touchedEntityPosition.y
+                touchPosition.z = touchedEntityPosition.z
+            }
+            break
+        case .right:
+            print("right")
+            touchPosition.x += boxSize/2
+            if minecraftMode {
+                touchPosition.y = touchedEntityPosition.y
+                touchPosition.z = touchedEntityPosition.z
+            }
+            break
+        default:
+            print("nothing")
+            touchPosition.y += boxSize/2
+            if minecraftMode {
+                touchPosition.x = touchedEntityPosition.x
+                touchPosition.z = touchedEntityPosition.z
+            }
+            break
         }
     }
     
     func placeCube(at position: SIMD3<Float>) {
         let origin = self.scene.anchors.first
         let box = ModelEntity(mesh: .generateBox(size: boxSize), materials: [gold])
-//        box.components.set(CollisionComponent(
-//            shapes: [.generateBox(size: [0.1, 0.1, 0.1])],
-//            mode: .default,
-//            filter: .default
-//        ))
         box.generateCollisionShapes(recursive: true)
         if minecraftMode {
             box.position.x = position.x
---
+++ Collaborative Building/Side.swift
@@ -0,0 +1,14 @@
+//
+//  Side.swift
+//  Collaborative Building
+//
+//  Created by Elias Lankinen on 11/24/20.
+//
+
+enum Side {
+    case up
+    case left
+    case front
+    case right
+    case back
+}
